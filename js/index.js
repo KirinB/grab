19713177ddd46cb15b65b37a06db9bc1605f4ce0
KirinB <lee.minhnhan29@gmail.com>
2024-08-13 11:26:05 +0700

    in hoa don

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function giaTienKmDauTien(loaiXe) {
   }
 }
 
-function giaTien1Den19(loaiXe, doanDuong) {
+function giaTien1Den19(loaiXe) {
   switch (loaiXe) {
     case XE_GRAB_CAR:
       return 7500;
@@ -30,7 +30,7 @@ function giaTien1Den19(loaiXe, doanDuong) {
   }
 }
 
-function giaTien19TroLen(loaiXe, doanDuong) {
+function giaTien19TroLen(loaiXe) {
   switch (loaiXe) {
     case XE_GRAB_CAR:
       return 7000;
@@ -40,6 +40,20 @@ function giaTien19TroLen(loaiXe, doanDuong) {
       return 9000;
     default:
       alert("Hệ thống có vấn đề vui lòng thử lại sau");
+      break;
+  }
+}
+
+function giaTienThoiGianCho(loaiXe) {
+  console.log(loaiXe);
+  switch (loaiXe) {
+    case XE_GRAB_CAR:
+      return "2.000";
+    case XE_GRAB_SUV:
+      return "3.000";
+    case XE_GRAB_BLACK:
+      return "3.500";
+    default:
       break;
   }
 }
@@ -71,14 +85,14 @@ document.getElementById("btnTinhTien").onclick = () => {
     if (soKm <= 19) {
       total =
         1 * giaTienKmDauTien(loaiXe.value) +
-        (soKm - 1) * giaTien1Den19(loaiXe.value, soKm) +
+        (soKm - 1) * giaTien1Den19(loaiXe.value) +
         tinhThoiGianCho(loaiXe.value, thoiGianCho);
       document.getElementById("divThanhTien").style = `display:block`;
     } else if (soKm > 19) {
       total =
         1 * giaTienKmDauTien(loaiXe.value) +
-        (soKm - 1) * giaTien1Den19(loaiXe.value, soKm) +
-        (soKm - 19) * giaTien19TroLen(loaiXe.value, soKm) +
+        (19 - 1) * giaTien1Den19(loaiXe.value) +
+        (soKm - 19) * giaTien19TroLen(loaiXe.value) +
         tinhThoiGianCho(loaiXe.value, thoiGianCho);
       document.getElementById("divThanhTien").style = `display:block`;
     }
@@ -89,4 +103,155 @@ document.getElementById("btnTinhTien").onclick = () => {
     "vi",
     { style: "currency", currency: "VND" }
   )}`;
+};
+
+function thongTinInHoaDon(loaiXe, soKm) {
+  $("#exampleModal").modal("show");
+  document.getElementById("loaiXe").innerHTML = loaiXe;
+  document.getElementById("soKm").innerHTML = `${soKm} km`;
+}
+
+function thoiGianChoInHoaDon(loaiXe, thoiGianCho) {
+  if (thoiGianCho <= 3) {
+    return `<td scope="row">Thời gian chờ <br /> <span class="small">3 phút đầu free</span></td>
+      <td>chờ ${thoiGianCho} phút</td>
+      <td>0 ₫</td>
+      <td>0 ₫</td>`;
+  } else {
+    return `<td scope="row">Thời gian chờ <br /> <span class="small">3 phút đầu free</span></td>
+      <td>chờ ${thoiGianCho} phút tính tiền ${thoiGianCho - 3}</td>
+      <td>${giaTienThoiGianCho(loaiXe.value) + " ₫"}</td>
+      <td>${tinhThoiGianCho(loaiXe.value, thoiGianCho).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>`;
+  }
+}
+
+document.getElementById("inHoaDon").onclick = () => {
+  //input
+  const soKm = document.getElementById("txt-km").value * 1;
+  const thoiGianCho = document.getElementById("txt-thoiGianCho").value * 1;
+  const loaiXe = document.querySelector("input[name=selector]:checked");
+  //handle
+  if (loaiXe === null || soKm === 0) {
+    alert("Vui lòng điền đầy đủ thông tin");
+  } else if (soKm === 1) {
+    $("#exampleModal").modal("show");
+    thongTinInHoaDon(loaiXe.value, soKm);
+    document.getElementById("kmDauTien").innerHTML = `
+      <td scope="row">KM đầu tiên</td>
+      <td>1km</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>`;
+    document.getElementById("thoiGianCho").innerHTML = thoiGianChoInHoaDon(
+      loaiXe,
+      thoiGianCho
+    );
+    document.getElementById("tongTien").innerHTML = `${(
+      1 * giaTienKmDauTien(loaiXe.value) +
+      tinhThoiGianCho(loaiXe.value, thoiGianCho)
+    ).toLocaleString("vi", {
+      style: "currency",
+      currency: "VND",
+    })}`;
+  } else if (soKm <= 19) {
+    $("#exampleModal").modal("show");
+    thongTinInHoaDon(loaiXe.value, soKm);
+    document.getElementById("kmDauTien").innerHTML = `
+      <td scope="row">KM đầu tiên</td>
+      <td>1km</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>`;
+    document.getElementById("tu1Den19").innerHTML = `
+      <td scope="row">Từ 1km đến ${soKm}km</td>
+      <td>${soKm - 1}km</td>
+      <td>${giaTien1Den19(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${((soKm - 1) * giaTien1Den19(loaiXe.value, soKm)).toLocaleString(
+        "vi",
+        {
+          style: "currency",
+          currency: "VND",
+        }
+      )}</td>
+      `;
+    document.getElementById("thoiGianCho").innerHTML = thoiGianChoInHoaDon(
+      loaiXe,
+      thoiGianCho
+    );
+    document.getElementById("tongTien").innerHTML = `${(
+      1 * giaTienKmDauTien(loaiXe.value) +
+      (soKm - 1) * giaTien1Den19(loaiXe.value) +
+      tinhThoiGianCho(loaiXe.value, thoiGianCho)
+    ).toLocaleString("vi", {
+      style: "currency",
+      currency: "VND",
+    })}`;
+  } else {
+    $("#exampleModal").modal("show");
+    thongTinInHoaDon(loaiXe.value, soKm);
+    document.getElementById("kmDauTien").innerHTML = `
+      <td scope="row">KM đầu tiên</td>
+      <td>1km</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${giaTienKmDauTien(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>`;
+    document.getElementById("tu1Den19").innerHTML = `
+      <td scope="row">Từ 1km đến 19km</td>
+      <td>18km</td>
+      <td>${giaTien1Den19(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${(18 * giaTien1Den19(loaiXe.value)).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      `;
+    document.getElementById("tu19TroLen").innerHTML = `
+      <td scope="row">Từ 19km trở lên</td>
+      <td>${soKm - 19}km</td>
+      <td>${giaTien19TroLen(loaiXe.value).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      <td>${((soKm - 19) * giaTien19TroLen(loaiXe.value)).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+      })}</td>
+      `;
+    document.getElementById("thoiGianCho").innerHTML = thoiGianChoInHoaDon(
+      loaiXe,
+      thoiGianCho
+    );
+    document.getElementById("tongTien").innerHTML = `${(
+      1 * giaTienKmDauTien(loaiXe.value) +
+      (19 - 1) * giaTien1Den19(loaiXe.value) +
+      (soKm - 19) * giaTien19TroLen(loaiXe.value) +
+      tinhThoiGianCho(loaiXe.value, thoiGianCho)
+    ).toLocaleString("vi", {
+      style: "currency",
+      currency: "VND",
+    })}`;
+  }
 };
